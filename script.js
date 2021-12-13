6ec57e31a87cad0cebae496c0fe0c26710da5d98
Douglas Aguiar <daguiaralcantara@gmail.com>
2021-12-13 10:29:45 -0300

    Resolução do sétimo requisito do projeto.

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 let input = document.querySelector('#texto-tarefa');
 let button = document.getElementById("criar-tarefa");
 let lista = document.getElementById("lista-tarefas");
-console.log(input);
 
-button.addEventListener('click', function() {
+button.addEventListener('click', criaTarefa);
+
+function criaTarefa() {
     let li = document.createElement('li');
+    li.className = 'item';
     li.innerText = input.value;
     lista.appendChild(li);
+    let item = document.querySelectorAll('.item');
     input.value = '';
-});
+    console.log(item);
+    for (tarefa of item) {
+        tarefa.addEventListener('click', mudaBackground);
+        function mudaBackground(event) {
+            let cor = document.querySelector('.clicado');
+            if (cor == true) {
+                cor.classList.remove('clicado');
+            }
+            let clicado = event.target;
+            clicado.className = 'clicado';
+        }
+    }
+};
+
+
+
