836d117a7d64ed2b38f8575298fe19de18676d12
Douglas Aguiar <daguiaralcantara@gmail.com>
2022-06-13 10:58:39 -0300

    Adição de estilo e um pouco de refatoração no código.

---
+++ README.md
@@ -0,0 +1,38 @@
+## Test Frontend: Douglas Aguiar
+This is my to do list project \o/
+Enjoy It!
+
+
+## Technology 
+
+Here are the technologies used in this project.
+
+* HTML5
+* CSS3
+* ECMAScript 6
+* Bootstrap versioon 5.2.0
+
+## Services Used
+
+* Github
+
+## Features
+
+The main features of the application are:
+ - Create many items to do
+ - Select the items done
+ - Save the list
+ - Delete the items done or selected or all items
+
+
+## Links
+  - Repository: https://github.com/douglasd18/to-do-list
+
+## Versioning
+
+  1.0.0.0
+
+
+## Authors
+
+  * **Douglas Aguiar** 
--- script.js
+++ script.js
@@ -1,25 +1,146 @@
-let input = document.querySelector('#texto-tarefa');
-let button = document.getElementById("criar-tarefa");
-let lista = document.getElementById("lista-tarefas");
-let apagaTudo = document.getElementById('apaga-tudo');
-let up = document.getElementById('mover-cima');
-let down = document.getElementById('mover-baixo');
-let removeFinalizadas = document.getElementById('remover-finalizados');
-let salvaTarefas = document.getElementById('salvar-tarefas');
-let removeSelected = document.getElementById('remover-selecionado');
+const input = document.querySelector('#texto-tarefa');
+const button = document.getElementById('criar-tarefa');
+const lista = document.getElementById('lista-tarefas');
+const apagaTudo = document.getElementById('apaga-tudo');
+const up = document.getElementById('mover-cima');
+const down = document.getElementById('mover-baixo');
+const removeFinalizadas = document.getElementById('remover-finalizados');
+const salvaTarefas = document.getElementById('salvar-tarefas');
+const removeSelected = document.getElementById('remover-selecionado');
 
-window.onload = function() {
-    lista.innerHTML = localStorage.getItem('tarefas');
-    let item = document.getElementsByClassName('item');
-    for (tarefa of item) {
-        tarefa.ondblclick = adicionaCompleted;
-        tarefa.addEventListener('click', mudaBackground);
-        function mudaBackground(event) {
-            removeBackground(item);
-            let clicado = event.target;
-            clicado.classList.add('clicado');
-        }
+function criaTarefa() {
+  const li = document.createElement('li');
+  li.className = 'item';
+  li.ondblclick = adicionaCompleted;
+  li.innerText = input.value;
+  lista.appendChild(li);
+  const item = document.querySelectorAll('.item');
+  input.value = '';
+  for (tarefa of item) {
+    tarefa.addEventListener('click', mudaBackground);
+    function mudaBackground(event) {
+      removeBackground(item);
+      const clicado = event.target;
+      clicado.classList.add('clicado');
     }
+  }
+}
+
+function removeBackground(item) {
+  for (const li of item) {
+    if (li.classList.contains('clicado')) {
+      li.classList.remove('clicado');
+    }
+  }
+}
+
+function adicionaCompleted(event) {
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
+}
+
+function apagaTarefas() {
+  lista.innerText = '';
+}
+
+function apagaFinalizados() {
+  const finalizados = document.getElementsByClassName('completed');
+  while (finalizados.length > 0) {
+    for (let i= 0; i < finalizados.length; i++) {
+      finalizados[i].remove();
+    }
+  }
+}
+
+function removeSelecionado() {
+  const clicado = document.querySelector('.clicado');
+  clicado.remove();
+}
+
+function salvarTarefas() {
+  localStorage.clear();
+  localStorage.setItem('tarefas', lista.innerHTML);
+}
+
+function changeText(text1, text2) {
+  const text = text1.innerText;
+  text1.innerText = text2.innerText;
+  text2.innerText = text;
+}
+
+function moveUp() {
+  const item = document.getElementsByClassName('item');
+  for (let i = 1; i < item.length; i++) {
+    if (item[i].classList.contains('clicado') && item[i].classList.contains('completed') && item[i - 1].classList.contains('completed')) {
+      changeText(item[i], item[i - 1]);
+      item[i].classList.remove('clicado');
+      item[i - 1].classList.add('clicado');
+    } else if (item[i].classList.contains('clicado') && item[i].classList.contains('completed')) {
+      changeText(item[i], item[i - 1]);
+      item[i].classList.remove('clicado');
+      item[i - 1].classList.add('clicado');
+      item[i].classList.remove('completed');
+      item[i - 1].classList.add('completed');
+    } else if (item[i].classList.contains('clicado') && item[i - 1].classList.contains('completed')) {
+      changeText(item[i], item[i - 1]);
+      item[i].classList.remove('clicado');
+      item[i - 1].classList.add('clicado');
+      item[i - 1].classList.remove('completed');
+      item[i].classList.add('completed');
+    } else if (item[i].classList.contains('clicado')) {
+      changeText(item[i], item[i - 1]);
+      item[i].classList.remove('clicado');
+      item[i - 1].classList.add('clicado');
+    }
+  }
+};
+
+function moveDown() {
+  const item = document.getElementsByClassName('item');
+  for (let i = 0; i < item.length - 1; i++) {
+    if (item[i].classList.contains('clicado') && item[i].classList.contains('completed') && item[i + 1].classList.contains('completed')) {
+      changeText(item[i + 1], item[i]);
+      item[i].classList.remove('clicado');
+      item[i + 1].classList.add('clicado');
+      break;
+    } else if (item[i].classList.contains('clicado') && item[i].classList.contains('completed')) {
+      changeText(item[i + 1], item[i]);
+      item[i].classList.remove('clicado');
+      item[i + 1].classList.add('clicado');
+      item[i].classList.remove('completed');
+      item[i + 1].classList.add('completed');
+      break;
+    } else if (item[i].classList.contains('clicado') && item[i + 1].classList.contains('completed')) {
+      changeText(item[i + 1], item[i]);
+      item[i].classList.remove('clicado');
+      item[i + 1].classList.add('clicado');
+      item[i + 1].classList.remove('completed');
+      item[i].classList.add('completed');
+      break;
+    } else if (item[i].classList.contains('clicado')) {
+      changeText(item[i + 1], item[i]);
+      item[i].classList.remove('clicado');
+      item[i + 1].classList.add('clicado');
+      break;
+    }
+  }
+}
+
+window.onload = () => {
+  lista.innerHTML = localStorage.getItem('tarefas');
+  const item = document.getElementsByClassName('item');
+  for (tarefa of item) {
+    tarefa.ondblclick = adicionaCompleted;
+    tarefa.addEventListener('click', mudaBackground);
+    function mudaBackground(event) {
+      removeBackground(item);
+      const clicado = event.target;
+      clicado.classList.add('clicado');
+    }
+  }
 };
 
 button.addEventListener('click', criaTarefa);
@@ -29,128 +150,3 @@ salvaTarefas.addEventListener('click', salvarTarefas);
 removeSelected.addEventListener('click', removeSelecionado);
 up.addEventListener('click', moveUp);
 down.addEventListener('click', moveDown);
-
-
-function criaTarefa() {
-    let li = document.createElement('li');
-    li.className = 'item';
-    li.ondblclick = adicionaCompleted;
-    li.innerText = input.value;
-    lista.appendChild(li);
-    let item = document.querySelectorAll('.item');
-    input.value = '';
-    for (tarefa of item) {
-        tarefa.addEventListener('click', mudaBackground);
-        function mudaBackground(event) {
-            removeBackground(item);
-            let clicado = event.target;
-            clicado.classList.add('clicado');
-        }
-    }
-};
-
-function removeBackground(item) {
-    for (const li of item) {
-        if (li.classList.contains('clicado')) {
-            li.classList.remove('clicado');
-        }
-    }
-}
-
-
-function adicionaCompleted(event){
-    if (event.target.classList.contains('completed')) {
-        event.target.classList.remove('completed')
-    } else {
-        event.target.classList.add('completed');
-    }
-}
-
-function apagaTarefas() {
-    while (lista.firstChild) {
-        lista.removeChild(lista.firstChild);
-    }
-}
-
-function apagaFinalizados() {
-    let finalizados = document.getElementsByClassName('completed');
-    while (finalizados.length > 0) {
-        for (let i = 0; i < finalizados.length; i++) {
-            finalizados[i].remove();
-        }
-    }
-}
-
-function removeSelecionado() {
-    let clicado = document.querySelector('.clicado');
-    clicado.remove();
-}
-
-function salvarTarefas() {
-    localStorage.clear();
-    localStorage.setItem('tarefas', lista.innerHTML);
-} 
-
-function changeText(text1, text2) {
-    let text = text1.innerText;
-    text1.innerText = text2.innerText;
-    text2.innerText = text;
-}
-
-function moveUp() {
-    let item = document.getElementsByClassName('item');
-    for (let i = 1; i < item.length; i++) {
-        if (item[i].classList.contains('clicado') && item[i].classList.contains('completed') && item[i - 1].classList.contains('completed')) {
-            changeText(item[i], item[i - 1]);
-            item[i].classList.remove('clicado');
-            item[i - 1].classList.add('clicado');
-        } else if (item[i].classList.contains('clicado') && item[i].classList.contains('completed')) {
-            changeText(item[i], item[i - 1]);
-            item[i].classList.remove('clicado');
-            item[i - 1].classList.add('clicado');
-            item[i].classList.remove('completed');
-            item[i - 1].classList.add('completed');
-        } else if (item[i].classList.contains('clicado') && item[i - 1].classList.contains('completed')) {
-            changeText(item[i], item[i - 1]);
-            item[i].classList.remove('clicado');
-            item[i - 1].classList.add('clicado');
-            item[i - 1].classList.remove('completed');
-            item[i].classList.add('completed');
-        }else if (item[i].classList.contains('clicado')) {
-            changeText(item[i], item[i - 1]);
-            item[i].classList.remove('clicado');
-            item[i - 1].classList.add('clicado');
-        }
-    }
-}
-
-function moveDown() {
-    let item = document.getElementsByClassName('item');
-    for (let i = 0; i < item.length - 1; i++) {
-        if (item[i].classList.contains('clicado') && item[i].classList.contains('completed') && item[i + 1].classList.contains('completed')) {
-            changeText(item[i + 1], item[i]);
-            item[i].classList.remove('clicado');
-            item[i + 1].classList.add('clicado');
-            break;
-        } else if (item[i].classList.contains('clicado') && item[i].classList.contains('completed')) {
-            changeText(item[i + 1], item[i]);
-            item[i].classList.remove('clicado');
-            item[i + 1].classList.add('clicado');
-            item[i].classList.remove('completed');
-            item[i + 1].classList.add('completed');
-            break;
-        } else if (item[i].classList.contains('clicado') && item[i + 1].classList.contains('completed')) {
-            changeText(item[i + 1], item[i]);
-            item[i].classList.remove('clicado');
-            item[i + 1].classList.add('clicado');
-            item[i + 1].classList.remove('completed');
-            item[i].classList.add('completed');
-            break;
-        }else if (item[i].classList.contains('clicado')) {
-            changeText(item[i + 1], item[i]);
-            item[i].classList.remove('clicado');
-            item[i + 1].classList.add('clicado');
-            break;
-        }
-    }
-}
